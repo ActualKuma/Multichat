46ab6f137b27279ce4daaa8b4c308a0acea94c62
ChocSprinklez <kieran@castlewalsh.com>
2024-04-06 15:40:12 +1100

    pretty terrible way to do it, but commands now display

--- multichat.js
+++ multichat.js
@@ -116,6 +116,100 @@ function openChats() {
                 chatbox.scrollTop = chatbox.scrollHeight;
         }
 
+        ComfyJS.onCommand = (user, message, flags, self, extra) => {
+            //Message Container
+            var newMessage = document.createElement("li");
+            newMessage.style.listStyleType = "none";
+
+            //Username
+            var username = document.createElement("div")
+            username.innerText = `${user}`;
+            username.style.color = `${extra.userColor}`;
+
+            if(extra.userColor == null) {
+                username.style.color = "#6441a5";
+            }
+
+            //Stream Identifier
+            var streamBadge = document.createElement("img");
+            streamBadge.src = `channel/${extra.channel}.png`;
+            streamBadge.id = "streambadge";
+            streamBadge.title = `${extra.channel}`;
+            newMessage.append(streamBadge);
+
+            //Add chat badges
+            const badgesJSON = extra.userBadges;
+            for(var key in extra.userBadges) {
+                if(key == "bits" || key == "broadcaster" || key == "founder" || key == "moderator" || key == "partner" || key == "sub-gift-leader" || key == "sub-gift-leader" || key == "sub-gifter" || key == "vip") {
+                    var badge = document.createElement("img");
+                    badge.src = `${key}/${badgesJSON[key]}.png`
+                    badge.id = "badge";
+                    badge.title = `${key} ${badgesJSON[key]}`;
+                    newMessage.append(badge);
+                } else if (key == "subscriber") {
+                    if(badgesJSON[key] > 12) {
+                        var badge = document.createElement("img");
+                        badge.src = `${key}/12.png`
+                        badge.id = "badge";
+                        badge.title = `${key} ${badgesJSON[key]}`;
+                        newMessage.append(badge);
+                    } else {
+                        var badge = document.createElement("img");
+                        badge.src = `${key}/${badgesJSON[key]}.png`
+                        badge.id = "badge";
+                        badge.title = `${key} ${badgesJSON[key]}`;
+                        newMessage.append(badge);
+                    }
+
+                }
+            }
+
+            //Message Seperator
+            var userMessageSeperator = document.createElement("div");
+            userMessageSeperator.innerText = ":" + String.fromCharCode(160);
+
+            //Message
+            var messageText = document.createElement("div");
+            messageText.innerText = `!${message}`;
+
+            //Add emotes to messages
+            const messageEmotes = extra.messageEmotes;
+            for(var key in extra.messageEmotes) {
+                emoteLocationArray = messageEmotes[key];
+
+
+                locations= emoteLocationArray[0].split('-');
+                
+                var x = Number(locations[0]);
+                var y = Number(locations[1]) + 1;
+                var replace = message.slice(x, y);
+
+                messageText.innerHTML = messageText.innerHTML.replaceAll(replace, `<img src="https://static-cdn.jtvnw.net/emoticons/v2/${key}/default/dark/1.0" id="emote" title="${replace}">`);
+            }
+
+            //turn links into hyperlinks
+            if(message.includes("http://") || message.includes("https://")) {
+                var messageWords = [];
+                messageWords = message.split(" ");
+                for(var w = 0; w < messageWords.length; w++) {
+                    if(messageWords[w].includes("http://") || messageWords[w].includes("https://")) {
+                        messageText.innerHTML = messageText.innerHTML.replaceAll(messageWords[w], `<a href="${messageWords[w]}">${messageWords[w]}</a>`);
+                    }
+                }
+            }
+
+            //Add message text
+            newMessage.append(username);
+            newMessage.append(userMessageSeperator);
+            newMessage.append(messageText);
+
+            chat.append(newMessage);
+
+            //Scroll chat to bottom
+            var chatbox = document.getElementById("scrollable")
+            chatbox.scrollTop = chatbox.scrollHeight;
+    }
+
         ComfyJS.Init(channels[i]);
     }
 
