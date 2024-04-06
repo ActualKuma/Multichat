f3c4cde1b787cc4445ab01b7953bcf55653c75a7
ChocSprinklez <kieran@castlewalsh.com>
2024-04-06 23:48:43 +1100

    delete messages

--- multichat.js
+++ multichat.js
@@ -35,71 +35,65 @@ function openChats() {
 
     for(var i = 0; i < channels.length ; i++) {
         ComfyJS.onChat = (user, message, flags, self, extra) => {
-                //Message Container
-                var newMessage = document.createElement("li");
-                newMessage.style.listStyleType = "none";
+            //Message Container
+            var newMessage = document.createElement("li");
+            newMessage.style.listStyleType = "none";
 
-                //Username
-                var username = document.createElement("div")
-                if(user.toLowerCase() != extra.username.toLowerCase()) {
-                    username.innerText = `${user}(${extra.username})`;
-                } else {
-                    username.innerText = `${user}`;
+            newMessage.id = `${extra.id}`;
+
+            //Username
+            var username = document.createElement("div")
+            if(user.toLowerCase() != extra.username.toLowerCase()) {
+                username.innerText = `${user}(${extra.username})`;
+            } else {
+                username.innerText = `${user}`;
+            }
+            
+
+            if(extra.userColor == null) {
+                username.style.color = "#6441a5";
+            } else {
+                var colour = tinycolor(`${extra.userColor}`);
+                if(colour.getBrightness() < 50) {
+                    colour.lighten();
+                    username.style.textShadow = "1px 1px 1px #ffffff"
                 }
-                
+                username.style.color = colour.toString();
+            }
 
-                if(extra.userColor == null) {
-                    username.style.color = "#6441a5";
-                } else {
-                    var colour = tinycolor(`${extra.userColor}`);
-                    if(colour.getBrightness() < 50) {
-                        colour.lighten();
-                        username.style.textShadow = "1px 1px 1px #ffffff"
-                    }
-                    username.style.color = colour.toString();
-                }
+            //Stream Identifier
+            const url = `channel/${extra.channel}.png`;
+            fetch(url, { method: "HEAD" }) 
+            .then(response => { 
+                if (response.ok) { 
+                    var streamBadge = document.createElement("img");
+                    streamBadge.src = url;
+                    streamBadge.id = "streambadge";
+                    streamBadge.title = `${extra.channel}`;
+                    newMessage.insertAdjacentElement("afterbegin", streamBadge);
+                } else { 
+                    var streamBadge = document.createElement("div");
+                    streamBadge.innerText = extra.channel[0] + extra.channel[1];
+                    streamBadge.id = "streambadge";
+                    streamBadge.style.fontWeight = "bold";
+                    newMessage.insertAdjacentElement("afterbegin", streamBadge);
+                } 
+            }) 
+            .catch(error => { 
+                console.log("An error occurred: ", error); 
+            }); 
+            
 
-                //Stream Identifier
-                const url = `channel/${extra.channel}.png`;
-                fetch(url, { method: "HEAD" }) 
-                .then(response => { 
-                    if (response.ok) { 
-                        var streamBadge = document.createElement("img");
-                        streamBadge.src = url;
-                        streamBadge.id = "streambadge";
-                        streamBadge.title = `${extra.channel}`;
-                        newMessage.insertAdjacentElement("afterbegin", streamBadge);
-                    } else { 
-                        var streamBadge = document.createElement("div");
-                        streamBadge.innerText = extra.channel[0] + extra.channel[1];
-                        streamBadge.id = "streambadge";
-                        streamBadge.style.fontWeight = "bold";
-                        newMessage.insertAdjacentElement("afterbegin", streamBadge);
-                    } 
-                }) 
-                .catch(error => { 
-                    console.log("An error occurred: ", error); 
-                }); 
-                
-
-                //Add chat badges
-                const badgesJSON = extra.userBadges;
-                for(var key in extra.userBadges) {
-                    if (key == "subscriber") {
-                        if(badgesJSON[key] > 12) {
-                            var badge = document.createElement("img");
-                            badge.src = `${key}/12.png`
-                            badge.id = "badge";
-                            badge.title = `${key} ${badgesJSON[key]}`;
-                            newMessage.append(badge);
-                        } else {
-                            var badge = document.createElement("img");
-                            badge.src = `${key}/${badgesJSON[key]}.png`
-                            badge.id = "badge";
-                            badge.title = `${key} ${badgesJSON[key]}`;
-                            newMessage.append(badge);
-                        }
-
+            //Add chat badges
+            const badgesJSON = extra.userBadges;
+            for(var key in extra.userBadges) {
+                if (key == "subscriber") {
+                    if(badgesJSON[key] > 12) {
+                        var badge = document.createElement("img");
+                        badge.src = `${key}/12.png`
+                        badge.id = "badge";
+                        badge.title = `${key} ${badgesJSON[key]}`;
+                        newMessage.append(badge);
                     } else {
                         var badge = document.createElement("img");
                         badge.src = `${key}/${badgesJSON[key]}.png`
@@ -107,52 +101,60 @@ function openChats() {
                         badge.title = `${key} ${badgesJSON[key]}`;
                         newMessage.append(badge);
                     }
+
+                } else {
+                    var badge = document.createElement("img");
+                    badge.src = `${key}/${badgesJSON[key]}.png`
+                    badge.id = "badge";
+                    badge.title = `${key} ${badgesJSON[key]}`;
+                    newMessage.append(badge);
                 }
+            }
 
-                //Message Seperator
-                var userMessageSeperator = document.createElement("div");
-                userMessageSeperator.innerText = ":" + String.fromCharCode(160);
+            //Message Seperator
+            var userMessageSeperator = document.createElement("div");
+            userMessageSeperator.innerText = ":" + String.fromCharCode(160);
 
-                //Message
-                var messageText = document.createElement("div");
-                messageText.innerText = `${message}`;
+            //Message
+            var messageText = document.createElement("div");
+            messageText.innerText = `${message}`;
 
-                //Add emotes to messages
-                const messageEmotes = extra.messageEmotes;
-                for(var key in extra.messageEmotes) {
-                    emoteLocationArray = messageEmotes[key];
+            //Add emotes to messages
+            const messageEmotes = extra.messageEmotes;
+            for(var key in extra.messageEmotes) {
+                emoteLocationArray = messageEmotes[key];
 
 
-                    locations= emoteLocationArray[0].split('-');
-                    
-                    var x = Number(locations[0]);
-                    var y = Number(locations[1]) + 1;
-                    var replace = message.slice(x, y);
+                locations= emoteLocationArray[0].split('-');
+                
+                var x = Number(locations[0]);
+                var y = Number(locations[1]) + 1;
+                var replace = message.slice(x, y);
 
-                    messageText.innerHTML = messageText.innerHTML.replaceAll(replace, `<img src="https://static-cdn.jtvnw.net/emoticons/v2/${key}/default/dark/1.0" id="emote" title="${replace}">`);
-                }
+                messageText.innerHTML = messageText.innerHTML.replaceAll(replace, `<img src="https://static-cdn.jtvnw.net/emoticons/v2/${key}/default/dark/1.0" id="emote" title="${replace}">`);
+            }
 
-                //turn links into hyperlinks
-                if(message.includes("http://") || message.includes("https://")) {
-                    var messageWords = [];
-                    messageWords = message.split(" ");
-                    for(var w = 0; w < messageWords.length; w++) {
-                        if(messageWords[w].includes("http://") || messageWords[w].includes("https://")) {
-                            messageText.innerHTML = messageText.innerHTML.replaceAll(messageWords[w], `<a href="${messageWords[w]}">${messageWords[w]}</a>`);
-                        }
+            //turn links into hyperlinks
+            if(message.includes("http://") || message.includes("https://")) {
+                var messageWords = [];
+                messageWords = message.split(" ");
+                for(var w = 0; w < messageWords.length; w++) {
+                    if(messageWords[w].includes("http://") || messageWords[w].includes("https://")) {
+                        messageText.innerHTML = messageText.innerHTML.replaceAll(messageWords[w], `<a href="${messageWords[w]}">${messageWords[w]}</a>`);
                     }
                 }
+            }
 
-                //Add message text
-                newMessage.append(username);
-                newMessage.append(userMessageSeperator);
-                newMessage.append(messageText);
+            //Add message text
+            newMessage.append(username);
+            newMessage.append(userMessageSeperator);
+            newMessage.append(messageText);
 
-                chat.append(newMessage);
+            chat.append(newMessage);
 
-                //Scroll chat to bottom
-                var chatbox = document.getElementById("scrollable")
-                chatbox.scrollTop = chatbox.scrollHeight;
+            //Scroll chat to bottom
+            var chatbox = document.getElementById("scrollable")
+            chatbox.scrollTop = chatbox.scrollHeight;
         }
 
         ComfyJS.onCommand = (user, message, flags, self, extra) => {
@@ -274,7 +276,12 @@ function openChats() {
             //Scroll chat to bottom
             var chatbox = document.getElementById("scrollable")
             chatbox.scrollTop = chatbox.scrollHeight;
-    }
+        }
+
+        ComfyJS.onMessageDeleted = (id, extra) => {
+            var todelete = document.getElementById(`${id}`);
+            todelete.style.display = "none";
+        }
 
         ComfyJS.Init(channels[i]);
     }
