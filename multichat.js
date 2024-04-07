661192693759b790ddd83f866d8e731f9d0ba124
ChocSprinklez <kieran@castlewalsh.com>
2024-04-07 18:55:12 +1000

    3rd party emotes not possible at this time

--- multichat.js
+++ multichat.js
@@ -39,12 +39,277 @@ function openChats() {
     
     var channels = []
 
+    var loadedAssets = {};
+
     let input = document.getElementById("streams").value;
     channels = input.split(" ");
 
     var chat = document.querySelector("#chat>ul");
 
     for(var i = 0; i < channels.length ; i++) {
+
+        var channel = channels[i];
+
+        /**
+         * Third Party Emote Integration taken From tmi-emote-pass by smilefx
+         * 
+         * https://github.com/smilefx/tmi-emote-parse
+         */
+
+        loadedAssets[channel] = {
+            channel: channel,
+            uid: "",
+            emotes: [],
+            badges: {},
+            badgesLoaded: [false, false, false],
+            allLoaded: false,
+            loaded: {
+                "bttv": {
+                    global: false,
+                    channel: false
+                },
+                "ffz": {
+                    global: false,
+                    channel: false
+                },
+                "7tv": {
+                    global: false,
+                    channel: false
+                }
+            }
+        }
+
+        if(enableFFZ) {
+            fetch(`https://api.frankerfacez.com/v1/room/${channel}`)
+            .then(response => response.json())
+            .then(body => {
+                try {
+                    Object.keys(body.sets).forEach(el => {
+                        var e = body.sets[el];
+
+                        e.emoticons.forEach(ele => {
+                            ele.code = ele.name;
+                            ele.type = "ffz";
+                            loadedAssets[channel].emotes.push(ele);
+                        })
+                    })
+
+                    checkLoadedAll(channel, "ffz", "channel", true, args);
+                    if (loadedAssets[channel].allLoaded) {
+                        loadedAssets[channel].emotes = loadedAssets[channel].emotes.sort(compareLength);
+                        exports.events.emit('emotes', {
+                            channel: channel
+                        });
+
+                        if (loadedAssets[channel].badgesLoaded[2]) {
+                            exports.events.emit('loaded', {
+                                channel: channel
+                            });
+                        }
+                    }
+
+                } catch (error) {
+                   console.log(error);
+                }
+            });
+
+            fetch(`https://api.frankerfacez.com/v1/set/global`)
+            .then(response => response.json())
+            .then(body => {
+                try {
+                    Object.keys(body.sets).forEach(el => {
+                        var e = body.sets[el];
+
+                        e.emoticons.forEach(ele => {
+                            ele.code = ele.name;
+                            ele.type = "ffz";
+                            loadedAssets[channel].emotes.push(ele);
+                        })
+                    })
+
+                    checkLoadedAll(channel, "ffz", "global", true, args);
+                    if (loadedAssets[channel].allLoaded) {
+                        loadedAssets[channel].emotes = loadedAssets[channel].emotes.sort(compareLength);
+                        exports.events.emit('emotes', {
+                            channel: channel
+                        });
+
+                        if (loadedAssets[channel].badgesLoaded[2]) {
+                            exports.events.emit('loaded', {
+                                channel: channel
+                            });
+                        }
+                    }
+                } catch (error) {
+                    console.log(error);
+
+                }
+
+            });
+
+            console.log(`ffz enabled for ${channel}`);
+        }
+
+
+        if(enableBTTV) {
+            fetch(`https://api.betterttv.net/3/cached/users/twitch/${uid}`)
+            .then(response => response.json())
+            .then(body => {
+                try {
+                    body.channelEmotes.forEach(ele => {
+                        ele.type = "bttv";
+                        loadedAssets[channel].emotes.push(ele);
+                    })
+
+                    body.sharedEmotes.forEach(ele => {
+                        ele.type = "bttv";
+                        loadedAssets[channel].emotes.push(ele);
+                    })
+
+                    checkLoadedAll(channel, "bttv", "channel", true, args);
+                    if (loadedAssets[channel].allLoaded) {
+                        loadedAssets[channel].emotes = loadedAssets[channel].emotes.sort(compareLength);
+                        exports.events.emit('emotes', {
+                            channel: channel
+                        });
+
+                        if (loadedAssets[channel].badgesLoaded[2]) {
+                            exports.events.emit('loaded', {
+                                channel: channel
+                            });
+                        }
+                    }
+                } catch (error) {
+                    console.log(error);
+
+                }
+            });
+
+            fetch(`https://api.betterttv.net/3/cached/emotes/global`)
+            .then(response => response.json())
+            .then(body => {
+                try {
+                    body.forEach(ele => {
+                        ele.type = "bttv";
+                        loadedAssets[channel].emotes.push(ele);
+                    })
+
+                    checkLoadedAll(channel, "bttv", "global", true, args);
+                    if (loadedAssets[channel].allLoaded) {
+                        loadedAssets[channel].emotes = loadedAssets[channel].emotes.sort(compareLength);
+                        exports.events.emit('emotes', {
+                            channel: channel
+                        });
+
+                        if (loadedAssets[channel].badgesLoaded[2]) {
+                            exports.events.emit('loaded', {
+                                channel: channel
+                            });
+                        }
+                    }
+                } catch (error) {
+                    console.log(error);
+
+                }
+            });
+            console.log(`bttv enabled for ${channel}`);
+        }
+
+        if(enable7TV) {
+            fetch(`https://api.7tv.app/v3/users/${channel}`)
+            .then(response => response.json())
+            .then(body => {
+                try {
+                    if (body.Status == undefined && body.Status != 404) {
+                        if (args["7tv"]["channel"] == true) {
+                            fetch(`https://api.7tv.app/v3/users/${channel}/emotes`)
+                                .then(response => response.json())
+                                .then(body => {
+                                    try {
+                                        if (body.Status == undefined && body.Status != 404) {
+                                            body.forEach(ele => {
+                                                ele.code = ele.name;
+                                                ele.type = "7tv";
+                                                loadedAssets[channel].emotes.push(ele);
+                                            })
+
+                                            checkLoadedAll(channel, "7tv", "channel", true, args);
+                                            if (loadedAssets[channel].allLoaded) {
+                                                loadedAssets[channel].emotes = loadedAssets[channel].emotes.sort(compareLength);
+                                                exports.events.emit('emotes', {
+                                                    channel: channel
+                                                });
+
+                                                if (loadedAssets[channel].badgesLoaded[2]) {
+                                                    exports.events.emit('loaded', {
+                                                        channel: channel
+                                                    });
+                                                }
+                                            }
+                                        } else {
+                                            console.log(error);
+
+
+                                            checkLoadedAll(channel, "7tv", "channel", true, args);
+                                        }
+                                    } catch (error) {
+                                        console.log(error);
+
+                                    }
+                                });
+                        } else {
+                            checkLoadedAll(channel, "7tv", "channel", null, args);
+                        }
+
+                        if (args["7tv"]["global"] == true) {
+                            fetch(`https://api.7tv.app/v3/emotes/global`)
+                                .then(response => response.json())
+                                .then(body => {
+                                    try {
+                                        body.forEach(ele => {
+                                            ele.code = ele.name;
+                                            ele.type = "7tv";
+                                            loadedAssets[channel].emotes.push(ele);
+                                        })
+
+                                        checkLoadedAll(channel, "7tv", "global", true, args);
+                                        if (loadedAssets[channel].allLoaded) {
+                                            loadedAssets[channel].emotes = loadedAssets[channel].emotes.sort(compareLength);
+                                            exports.events.emit('emotes', {
+                                                channel: channel
+                                            });
+
+                                            if (loadedAssets[channel].badgesLoaded[2]) {
+                                                exports.events.emit('loaded', {
+                                                    channel: channel
+                                                });
+                                            }
+                                        }
+                                    } catch (error) {
+                                        console.log(error);
+
+                                    }
+                                });
+                        } else {
+                            checkLoadedAll(channel, "7tv", "global", null, args);
+                        }
+                    } else {
+                        console.log(error);
+
+
+                        checkLoadedAll(channel, "7tv", "channel", true, args);
+                        checkLoadedAll(channel, "7tv", "global", true, args);
+                    }
+                } catch (error) {
+                    console.log(error);
+
+                }
+            });
+            console.log(`7tv enabled for ${channel}`);
+        }
+
+        console.log(loadedAssets);
+
         ComfyJS.onChat = (user, message, flags, self, extra) => {
             if(!ignoreUsers.includes(extra.username)) {
                 //Message Container
@@ -305,4 +570,31 @@ function openChats() {
 
     var form = document.getElementById("frm1");
     form.style.display = "none";
+}
+
+function checkLoadedAll(channel, type, extra, value, args) {
+    if (args[type][extra] == false && value == null) {
+        loadedAssets[channel].loaded[type][extra] = null;
+    }
+    if (args[type][extra] == true && loadedAssets[channel].loaded[type][extra] == false && value == true) {
+        loadedAssets[channel].loaded[type][extra] = true;
+    }
+
+    var trueVals = [];
+    Object.keys(loadedAssets[channel].loaded).forEach((e, ind) => {
+        e = loadedAssets[channel].loaded[e];
+        var allTrue = true;
+        Object.keys(e).forEach(ele => {
+            ele = e[ele];
+            if (ele == false) {
+                allTrue = false;
+            }
+        })
+
+        trueVals.push(allTrue);
+    });
+
+    loadedAssets[channel].allLoaded = !trueVals.includes(false);
+    return !trueVals.includes(false);
+
 }
